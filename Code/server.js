6dd87742a29dc7d7441cf8b472250967c0ed588a
Francie <Francie.Roessler@web.de>
2019-10-18 15:10:02 +0200

    First Upload
    Code

---
+++ Code/server.js
@@ -0,0 +1,103 @@
+/*Installation und Initialisierung aller Packages und Module (express, body-parser, ejs, sqlite, cookie-parser, express-session)*/
+
+const express = require('express');
+const app =  express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.engine('.ejs', require('ejs').__express);
+app.set('view engine','ejs');
+
+app.listen(3000, function(){
+    console.log('listening on 3000');
+});
+
+//Initialisierung des Cookie Parsers
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+//initialisierung von express-sessions
+const session = require('express-session');
+app.use(session({
+    secret: "example",
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+/////////////////* FREIGABEN *///////////////////
+
+//Freigabe des public Ordners
+app.use(express.static(__dirname + '/public'));
+
+
+
+//////////////////////* DATENBANKEN *//////////////////////
+// Verbindung zur Datenbank
+const sqlite3 = require('sqlite3').verbose();
+
+//Datenbank fürs Login aufgesetzt
+let db = new sqlite3.Database('login.db', function(err) {
+    if (err) { 
+        console.error(err); 
+    } else {
+        console.log("Verbindung zur Datenbank wurde erfolgerich hergestellt.")
+    }
+});
+
+
+
+///////////////////*AUSGABEN*///////////////////////
+app.get('/main', function(req,res){
+    res.render('main');
+});
+
+//Ausgabe des Registrieren Formulars
+app.get('/signup', function(req, res){
+    res.render('signup');
+});
+
+//Ausgabe des Logins
+app.get('/login', function(req,res){
+    res.render('login');
+});
+
+///////////////////*AUSWERTUNGEN*/////////////////////////
+//Post Auswertung des logins
+app.post('/doLogin', function(req,res){
+    const name = req.body.name;
+    const password = req.body.password;
+
+    let sql = `SELECT * FROM user WHERE name="${name}"`;
+    db.get(sql, function(err, row){
+        //wir wollen username&email in ner Session Variable speichern
+        req.session.name = row.name;
+        //Session Variablen sollen in die loginresponse.ejs übergeben wrden
+        res.render('loginresponse', {
+            name: req.session.name, 
+           
+        });
+    });
+});
+    
+
+//Auswertung nach der Registrierung
+app.post('/doRegister', function(req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //SQL Befehl um einen neuen Eintrag der Tabelle user hinzuzufügen
+    let sql = `INSERT INTO user (name, email, password) VALUES ("${name}", "${email}", "${password}");`
+    db.run(sql, function(err) {
+        if (err) { 
+            console.error(err)
+        } else {
+            res.send('Benutzer wurde angelegt');
+        }
+    });
+   
+});
+
+/////////////////*FUNKTIONEN*///////////////////////////////
