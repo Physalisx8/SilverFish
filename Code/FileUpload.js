3659ab1443c6656a89a2202a8c2e47e55f788f79
BigBadSheep <j.antich11@gmai.com>
2019-10-30 12:51:49 +0100

    Foto Uploads
    Foto Uploads (Mona ist kaka).

---
+++ Code/FileUpload.js
@@ -0,0 +1,30 @@
+// First install: bopdy-parser, formidable, express
+const express = require("express");
+const app = express();
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+//Open the Fileuploading html
+app.get('/Files', function(req,res){
+  res.sendfile(__dirname + "/Files.html");
+});
+
+//Post von der Fileupload IN server
+app.post('/fileupload', function (req, res) {
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/jesus/Documents/HAW/Semester 2/Angewandte Programmierung/AP Projekt' + files.filetoupload.name;  //Name(Präfix) und speicher verzeichnis von Bild
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+});
+app.listen(3000, function(){
+    console.log("Listening on Port 3000");
+});
